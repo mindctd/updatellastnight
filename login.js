482efb63f29527079ea442476ee7597330f998e0
mindctd <mindstory483@gmail.com>
2024-05-07 01:11:58 +0700

    first commit

--- login.js
+++ login.js
@@ -1,48 +1,61 @@
-const mysql = require("mysql");
-const express = require("express");
-const bodyParser = require("body-parser");
-const encoder = bodyParser.urlencoded();
-const app = express();
+var mysql = require("mysql");
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "accounts",
+});
+var app = express();
 app.use("/login.css", express.static("login.css"));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1234",
-    database: "customer2",
-    port: "3306"
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname + "/login.html"));
+});
+app.post('/auth', function (request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+  if (username && password) {
+    connection.query(
+    'select * from accounts where username=? and password =?',
+      [username, password],
+      function (error, results, fields) {
+        if (results.lenght > 0) {
+          request.session.loggedin = true;
+          request.session.username = username;
+          response.redirect('/home');
+        } else {
+          response.send('Incorrect Username and/or Password');
+        }
+        response.end();
+      }
+    );
+  } else {
+    request.send('Please enter Username and Password');
+    response.end();
+  }
 });
 
-connection.connect(function(error) {
-    if (error) {
-        console.error("Error connecting to database:", error);
-    } else {
-        console.log("Connected to database successfully!!");
+app.get('/home', function(request,response){
+    if (request.session.loggedin){
+        response.send('Welcome,'+ request.session.username + '!');
+
+    }else{
+        response.send('Please login to view this page');
     }
+     response.end();
 });
-
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/login.html");
-});
-
-app.post("/", encoder, function(req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    connection.query("SELECT * FROM loginuser WHERE user_name = ? AND user_pass = ?", [username, password], function(error, results, fields) {
-  
-            if (results.length > 0) {
-                res.redirect("/welcome");
-            } else {
-                res.redirect("/");
-            }
-            res.end();
-        })
-    });
-
-
-app.get("/index.html", function(req, res) {
-    res.sendFile(__dirname + "/welcome.html");
-})
-
-app.listen(4500);
+app.listen(3000);
